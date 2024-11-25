e730106669385398e786e89bbb5102fbca0b9397
rumiyevskiy <rumiyevskiy@gmail.com>
2024-11-25 12:42:33 +0200

    modified:   css/style.css 	modified:   index.html 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -194,9 +194,147 @@ document.addEventListener("DOMContentLoaded", function () {
         wordIndex = 0;
       }
     }
+  };
+
+  
+  // ***********************************************************************
+
+  const popupLinks = document.querySelectorAll('.popup-link');
+  const body = document.querySelector('body');
+  const lockPadding = document.querySelectorAll('.lock-padding');
+
+  let unlock = true;
+
+  const timeout = 300;
+
+  if (popupLinks.length > 0) {
+      for (let index = 0; index < popupLinks.length; index++) {
+          const popupLink = popupLinks[index];
+          popupLink.addEventListener("click", function (e) {
+              const popupName = popupLink.getAttribute('href').replace('#', '');
+              const curentPopup = document.getElementById(popupName);
+              console.log("curentPopup: ", curentPopup);
+              popupOpen(curentPopup);
+              e.preventDefault();
+          });
+      }
+  };
+  
+  const popupCloseIcon = document.querySelectorAll('.close-popup');
+  if (popupCloseIcon.length > 0) {
+      for (let index = 0; index < popupCloseIcon.length; index++) {
+          const el = popupCloseIcon[index];
+          el.addEventListener("click", function (e) {
+              popupClose(el.closest('.popup'));
+              e.preventDefault();
+          });
+      }
+  };
+
+  function popupOpen(curentPopup) {
+      if (curentPopup && unlock) {
+          const popupActive = document.querySelector('.popup.open');
+          if (popupActive) {
+              popupClose(popupActive, false);
+          } else {
+              bodyLock();
+          }
+          curentPopup.classList.add('open');
+          curentPopup.addEventListener("click", function (e) {
+              if (!e.target.closest('.popup__content')) {
+                  popupClose(e.target.closest('.popup'));
+              }
+          });
+       };
+  };
+
+  function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        
+      const videoElem = document.getElementById("videoPlayer");
+      // videoElem.style.display = "none";
+
+      document.getElementById("videoPlayer").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+      console.log("popup closed", videoElem);
+
+        popupActive.classList.remove('open');
+        
+          if (doUnlock) {
+              bodyUnLock();
+          }
+      }
+   };
+  
+  function bodyLock() {
+      // const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+      // if (lockPadding.length > 0) {
+      //     for (let index = 0; index < lockPadding.length; index++) {
+      //         const el = lockPadding[index];
+      //         el.style.paddingRight = lockPaddingValue;
+      //     }
+      // }        
+      // body.style.paddingRight = lockPaddingValue;
+      body.classList.add('__lock');
+
+      unlock = false;
+      setTimeout(function () {
+          unlock = true;
+      }, timeout);
   }
 
-    // *************************************************************
+  function bodyUnLock() {
+      setTimeout(function () {
+          // if (lockPadding.length > 0) {
+          //     for (let index = 0; index < lockPadding.length; index++) {
+          //         const el = lockPadding[index];
+          //         el.style.paddingRight = '0px';
+          //     }
+          // }            
+          // body.style.paddingRight = '0px';
+          body.classList.remove('__lock');
+      }, timeout);
+
+      unlock = false;
+      setTimeout(function () {
+          unlock = true;
+      }, timeout);
+  }
+
+  document.addEventListener('keydown', function (e) {
+      if (e.key === 'Escape') {
+          const popupActive = document.querySelector('.popup.open');
+          popupClose(popupActive);
+      }
+  })
+
+  // **************************************************************************
+  //   let player;
+
+  //   // Ініціалізація YouTube API
+  //   function onYouTubeIframeAPIReady() {
+  //       player = new YT.Player('videoPlayer', {
+  //           events: {
+  //           'onReady': onPlayerReady
+  //           }
+            
+  //       });
+  //     console.log("player: ", player);
+  //   }
+
+  // function onPlayerReady(event) {
+  //   console.log("player2: ", player);
+  //       // Обробка натискання кнопки
+  //   document.querySelector('.close-popup').addEventListener('click', () => {
+  //     console.log("player3: ", player);
+  //           if (player) {
+  //               player.pauseVideo(); // Зупиняє відео
+  //           }
+  //       });
+  // };
+  // **************************************************************************
+  
+
 
 });
   
