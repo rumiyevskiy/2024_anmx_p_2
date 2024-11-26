ae1a0cf6b44fda08deb71fc116271e57da75573d
rumiyevskiy <rumiyevskiy@gmail.com>
2024-11-26 17:44:30 +0200

    modified:   css/style.css 	modified:   index.html 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -379,6 +379,298 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   
+  // ***********************************************************************
+
+  const page_1 = document.querySelector(".popup__content-page-1");
+  const page_2 = document.querySelector(".popup__content-page-2");
+  page_2.classList.add("pagehidden");
+  const page_3 = document.querySelector(".popup__content-page-3");
+  page_3.classList.add("pagehidden");
+
+  const selectNum = document.querySelector(".custom-select");
+  const optionsNum = selectNum.querySelectorAll(".option");
+  const hiddenInputNum = selectNum.querySelector("input[type=hidden]");
+
+  const selectСomposition = document.querySelector(".custom-select.__2"); 
+  const optionsСomposition = selectСomposition.querySelectorAll(".option");
+  const hiddenInputСomposition = selectСomposition.querySelector("input[type=hidden]#select_type-2");
+
+  const btnResetPage1 = document.querySelector(".popup__content-page-1 button.custom-btn[type=reset]");
+  const btnResetPage2 = document.querySelector(".popup__content-page-2 button.custom-btn[type=reset]");
+  const btnResetPage3 = document.querySelector(".popup__content-page-3 button.custom-btn[type=reset]");
+  const btnNextPage1 = document.querySelector(".popup__content-page-1 button.custom-btn.next");
+  const btnNextPage2 = document.querySelector(".popup__content-page-2 button.custom-btn.next");
+  const btnBackPage2 = document.querySelector(".popup__content-page-2 button.custom-btn.back");
+  const btnBackPage3 = document.querySelector(".popup__content-page-3 button.custom-btn.back");
+
+
+  optionsNum.forEach((option) => {
+    option.addEventListener("click", () => {
+      const value = option.getAttribute("data-value");
+      hiddenInputNum.value = value;
+
+      console.log("selectValue: ", hiddenInputNum.value);
+
+      optionsNum.forEach((opt) => opt.classList.remove("selected"));
+      option.classList.add("selected");
+    });
+  });
+
+  btnResetPage1.addEventListener("click", () => {
+    optionsNum.forEach((opt) => opt.classList.remove("selected"));
+    hiddenInputNum.value = "";
+  });
+
+  btnNextPage1.addEventListener("click", (event) => {
+
+    const selectedOpts = document.querySelectorAll(".popup__content-page-1 .custom-select.__1 .selected");
+
+    if (!selectedOpts.length) {
+      alert("Ці поля повині бути заповнені");
+      event.preventDefault();
+      return;
+    }
+
+    page_1.classList.add("pagehidden");
+    page_2.classList.remove("pagehidden");
+     event.preventDefault();
+    // return;
+
+  });
+
+  const selectedValues = new Set(); // Використовуємо Set для унікальності
+
+  optionsСomposition.forEach((option) => {
+    option.addEventListener("click", () => {
+      const value = option.getAttribute("data-value");
+
+      if (selectedValues.has(value)) {
+        // Якщо опція вже вибрана, видаляємо її
+        selectedValues.delete(value);
+        option.classList.remove("selected");
+      } else {
+        // Якщо опція не вибрана, додаємо її
+        selectedValues.add(value);
+        option.classList.add("selected");
+      }
+
+      // Оновлюємо значення прихованого інпуту
+      hiddenInputСomposition.value = Array.from(selectedValues).join(",");
+    });
+  });
+
+  btnResetPage2.addEventListener("click", () => {
+    optionsСomposition.forEach((opt) => opt.classList.remove("selected"));
+    hiddenInputСomposition.value = "";
+    console.log("selectValueCompose: ", hiddenInputСomposition.value);
+  });
+
+  btnNextPage2.addEventListener("click", (event) => {
+
+    const selectedOpts = document.querySelectorAll(".popup__content-page-2 .custom-select.__2 .selected");
+    console.log("page1: ", hiddenInputNum.value);
+    console.log("page2: ", hiddenInputСomposition.value);
+
+    if (!selectedOpts.length) {
+      alert("Ці поля повині бути заповнені");
+      event.preventDefault();
+      return;
+    }
+
+    page_2.classList.add("pagehidden");
+    page_3.classList.remove("pagehidden");
+     event.preventDefault();
+  });
+
+  btnBackPage2.addEventListener("click", (event) => {
+    page_2.classList.add("pagehidden");
+    page_1.classList.remove("pagehidden");
+    event.preventDefault();
+  });
+
+  btnBackPage3.addEventListener("click", (event) => {
+    page_3.classList.add("pagehidden");
+    page_2.classList.remove("pagehidden");
+    event.preventDefault();
+  });
+
+  // *****************************************************************
+
+  const mainForm = document.getElementById('form');
+
+  // Логіка обробки форми
+  mainForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Зупиняємо стандартну поведінку форми
+    if (!mainForm.privacy.checked) {
+      const privacy = document.querySelector(".privacy-policy");
+      privacy.classList.add("error");
+      alert("Заповніть всі поля!")
+      return;
+    }
+    const privacy = document.querySelector(".privacy-policy");
+    privacy.classList.remove("error");
+      sendTelegram(); // Викликаємо функцію для відправки в Telegram
+  });
+
+  // Функція для відправки повідомлення в Telegram
+  async function sendTelegram() {
+        
+
+    // let optionValueTypeServices = document.querySelector("#select_services");
+
+    // const selectedOption = optionValueTypeServices.options[optionValueTypeServices.selectedIndex];
+
+    // const dataTranslate = selectedOption.getAttribute('data-translate');
+
+    // let optionValueTypeServicesUkr = optionValueTypeServicesArr[dataTranslate];
+
+    const botToken = '7648355172:AAE4jsw4ZfadhgoEezXJyy0X7U4EQwFkkbQ'; // Токен МІЙ бота
+    const chatId = '-4588952109'; // ID чату
+    // const botToken = '7648355172:AAE4jsw4ZfadhgoEezXJyy0X7U4EQwFkkbQ'; // Токен бота
+    // const chatId = '-4588952109'; // ID чату
+    
+        const name = mainForm.name.value;
+        const phone = mainForm.phone.value;
+        const email = mainForm.email.value;
+        const request = hiddenInputNum.value;    
+        const select_type = hiddenInputСomposition.value;
+        const privacy = mainForm.privacy.checked ? 'Так' : 'Ні';
+
+        const bodymessage = `
+            Запит з сайту Annamax
+            Ім'я: ${name}
+            Телефон: ${phone}
+            Пошта: ${email}
+            Літери: ${request}
+            Наповнювачи: ${select_type}
+
+            Згода на обробку даних: ${privacy}
+        `;
+
+        // Відправка через API Telegram
+        try {
+            const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: bodymessage
+                })
+            });
+
+            const data = await response.json();
+            if (data.ok) {
+
+                // let msgTelegramGoodVar = translations[teleramsent];
+
+                // console.log(msgTelegramGoodVar);
+                console.log('The message has been successfully sent!');
+                // alert(msgTelegramGoodVar);
+                alert('Thank you! Your message has been sent.');
+
+                // Автоматичне надсилання повідомлення на пошту за допомогою PHP(треба перевіряти), повідомлення не бачно
+                // sendEmailPHP();
+
+                // Відкриття додатку по замовчуванню для підтвердження та надсилання повідомлення на пошту
+                // sendEmail(name, phone, email, request, select_type, select_services, privacy);
+
+                // Автоматичне надсилання повідомлення на пошту, повідомлення не бачно
+                sendEmail2();
+
+            } else {
+
+                // let msgTelegramErrVar = translations[errorTelegram];
+                
+                // console.error(msgTelegramErrVar, data);
+                console.error('Помилка Telegram:', data);
+                // alert(msgTelegramErrVar);
+                alert('An error occurred while sending the message');
+            }
+        } catch (error) {
+
+            // let msgReqErrVar = translations[reqErr];
+            // console.error(msgReqErrVar, error);
+            console.error('Помилка запиту:', error);
+            // alert(msgReqErrVar);
+            alert('An error occurred while sending the request.');
+        }
+  }
+  
+  function sendEmail2() {
+
+    // сайт: https://dashboard.emailjs.com/admin/account
+    // це Public Key з розділу account/general:API keys
+    let emailjsID = "_ruQbUC348SMI_KYA";
+
+    // ініціалізація сервісу за допомогою Public Key (або ще його називають user_id)
+    emailjs.init(emailjsID);
+
+    // тут я отриммав переклад деяких значень для формування об'єкту данних для відправки на пошту:
+    // знаходжу елемент select з id у формі
+    // let optionValueTypeServices = document.querySelector("#select_services");
+
+    // тут в елементі select отримаємо вибраний користувачем option за допомогою індекса вибраного (selectedIndex) , тобто ми взяли елемент select зі змінної optionValueTypeServices та за допомогою метода options звернулись до масиву усіх options, а в [] дужках за допомогою метода selectedIndex отримали цифру яка дорівнює індексу обраного елементу option
+    // const selectedOption = optionValueTypeServices.options[optionValueTypeServices.selectedIndex];
+
+    // тут ми отримаємо значення з атрибуту перекладу data-translate який є у елемента option отриманого вишче, це потрібно для формування перекладів в проекті
+    // const dataTranslate = selectedOption.getAttribute('data-translate');
+
+    // тут ми звертаємось до отриманого раніше об'єкту перекладів optionValueTypeServicesArr і з ньго отримаємо значення перекладу за ключем, отриманим за крок до цього: dataTranslate
+    // let optionValueTypeServicesUkr = optionValueTypeServicesArr[dataTranslate]; 
+
+    // тут ми отримаємо в змінні значення з елементів форми mainForm
+    // const name = mainForm.name.value;
+    // const phone = mainForm.phone.value;
+    // const email = mainForm.email.value;
+    // const request = mainForm.request.value;
+    // const select_type = mainForm.select_type.value;
+    // const select_services = optionValueTypeServicesUkr;
+    // const privacy = mainForm.privacy.checked ? 'Так' : 'Ні';
+
+    const name = mainForm.name.value;
+        const phone = mainForm.phone.value;
+        const email = mainForm.email.value;
+        const request = hiddenInputNum.value;    
+        const select_type = hiddenInputСomposition.value;
+        const privacy = mainForm.privacy.checked ? 'Так' : 'Ні';
+
+    // Параметри для Email.js, тут ми формуємо об'єкт, який надішлемо до пошти, вказаної при реєстрації на сервісі emailjs. тут головне: щоб назви ключей відповідали змінним у подвійних дужках {{}} в темплейті(шаблоні) в сервісі emailjs
+    const templateParams = {
+        // to_email: 'rumiyevskiy@gmail.com',
+        name: `${name} from site "Annamax" `,
+        phone: phone,
+        email: email,
+        comments: request,
+        carType: select_type,
+        // service: select_services,
+        privacy: privacy,
+    };
+
+    // сюда SERVICE_ID записується Service ID з вкладки Edit Service який ми отримали при додаванні сервіса, яким будемо користуватися при надсиланнях повідомлень в emailjs. я використовував gmail
+    let SERVICE_ID = 'service_oeydswb';
+    // сюда TEMPLATE_ID записується Template ID з вкладки Email Templates, далі обираємо потрібний створений нами template (в безкоштовному варіанті їх тільки два), далі обираємо settings, там знаходимо Template ID
+    let TEMPLATE_ID = 'template_2pd9prh';
+
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams)
+        .then((response) => {
+
+            // let msgGoodVar = translations[Emailsent];
+            // console.log(msgGoodVar, response.status, response.text);
+            console.log('Email успішно відправлено!', response.status, response.text);
+
+            // alert(msgGoodVar);
+            alert('Ваше повідомлення успішно відправлено!');
+        })
+        .catch((error) => {
+
+            // let msgErrVar = translations[error];
+            // console.error(msgErrVar, error);
+            console.error('Помилка відправки:', error);
+            // alert(msgErrVar);
+            alert('Сталася помилка при відправці.');
+        });
+}
+  
 
 
 });
